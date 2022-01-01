c5510854b44ef9011f5d9158a251acf4549baac6
younahshin-dev <roaving@gmail.com>
2022-01-01 16:51:24 +0900

    modify css

--- build/main.js
+++ build/main.js
@@ -1,3 +1,13 @@
-"use strict";
-console.log('dfdf');
+'use strict';
+class PageComponent {
+    constructor(type, contents) {
+        this.type = type;
+        this.contents = contents;
+    }
+}
+const buttons = document.querySelectorAll("button");
+//buttons.addEventListener("click", handleClick);
+function handleClick() {
+    console.log(this);
+}
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,18 @@
 'use strict'
 
+type ComponentType = 'image' | 'video' | 'Note' | 'Task';
+class PageComponent {
+  type:ComponentType;
+  contents:string;
+  constructor(type:ComponentType, contents:string) {
+    this.type = type;
+    this.contents = contents; 
+  }
+}
+
+const buttons= document.querySelectorAll("button");
+//buttons.addEventListener("click", handleClick);
+
+function handleClick() {
+  console.log(this);
+}
