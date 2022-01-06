b01b0884c84111f9554789ec9eecd460310c8e3f
younahshin-dev <roaving@gmail.com>
2022-01-06 13:54:44 +0900

    commit build folder

--- build/app.js
+++ build/app.js
@@ -1,11 +1,11 @@
-import { ImageComponent } from './components/image.js';
-import { PageComponent } from './components/page.js';
+import { ImageComponent } from './components/page/item/image.js';
+import { PageComponent } from './components/page/page.js';
 class App {
     constructor(appRoot) {
         this.page = new PageComponent();
         this.page.attachTo(appRoot);
-        this.image = new ImageComponent();
-        this.image.attachTo(appRoot);
+        this.image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
+        this.image.attachTo(appRoot, 'beforeend');
     }
 }
 new App(document.querySelector('.document'));
